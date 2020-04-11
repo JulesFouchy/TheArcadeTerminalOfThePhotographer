617c754914f7c72aa9ea09f61eae3350dd9407ee
Jules Fouchy <jules.fouchy@ntymail.com>
2020-04-11 21:58:40 +0200

    Add info logo and link a function on click

---
+++ explanations.js
@@ -0,0 +1,5 @@
+const EXPLANATIONS = {
+	saturation: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+	luminosity: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+	contrast: "ccccccccccccccccccccccccccccccccccccccccccc"
+}
--- slider.js
+++ slider.js
@@ -5,14 +5,18 @@ const SLIDERS = {
 }
 
 const createSliders = () => {
-	createSlider("saturation",  0, 2)
-	createSlider("luminosity", 0, 2)
-	createSlider("contrast", -1, 1)
+	createSlider('saturation',  0, 2)
+	createSlider('luminosity', 0, 2)
+	createSlider('contrast', -1, 1)
 }
 
 const setValue = (variableName, val) => {
 	SLIDERS[variableName] = val
 	document.getElementById(variableName+'Value').innerHTML = SLIDERS[variableName].toFixed(2)
+}
+
+const showInfos = (variableName) => {
+	console.log(EXPLANATIONS[variableName])
 }
 
 const createSlider = (variableName, min, max, initialValue) => {
@@ -21,6 +25,7 @@ const createSlider = (variableName, min, max, initialValue) => {
 	slidersContainer.innerHTML += '<span class = "sliderWrapper">'
 		slidersContainer.innerHTML += "<span>" + variableName + " </span>"
 		slidersContainer.innerHTML += '<span id = "'+ variableName +'Value">' + SLIDERS[variableName].toFixed(2) + '</span>'
+		slidersContainer.innerHTML += '<img class = "infoLogo" src = "img/info.png" onclick = "showInfos(\''+variableName+'\')" />'
 		slidersContainer.innerHTML += "<input type=\"range\" min=\""+ min*100 +"\" max=\""+ max*100 +"\" value=\""+ SLIDERS[variableName]*100 +"\" class=\"slider\" id=\""+ variableName +"\" "+onInputFunc + "\\>\n"
 	slidersContainer.innerHTML += '</span>'
 }
